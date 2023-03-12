7ff95b895b12cb4a8f1d258864f7cd9d1b023815
ochieng <cindycynthia387@gmail.com>
2023-03-13 00:21:07 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+let x= 1;
+let y= 80;
+
+function add(x, y){
+    return x + y
+}
+function subtract(x, y){
+    return x - y
+}
+function multiply(x, y){
+    return x * y
+}
+function divide(x, y){
+    return x / y
+}
+function increment(z){
+z++;
+return z
+}
+
+function decrement(w){
+    w--;
+    return w
+}
+
+function makeInt(n){
+   return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n, 1.01);
+}
